f13bfdb73786e4049b103c8a7fc6ef20ceb4abad
Benjamin Kipkorir Tonui <benjamin.tonui@mgas.ke>
2024-03-18 08:19:37 +0300

    initializing front end part

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <nav>
+
+      </nav>
+
+      <div class="row">
+        <div class="col-md-12">
+          <h1 id="header">Welcome To EmbTec Solution</h1>
+        </div>
+
+      </div>
+
     </div>
+
+  
   );
 }
 
